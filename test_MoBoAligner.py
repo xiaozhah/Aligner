57049c1b590ca55e8e4e8bedbcd393554fba54fb
xiaozhah <1298856981@qq.com>
2024-04-13 19:42:57 +0800

    add seed

--- test_MoBoAligner.py
+++ test_MoBoAligner.py
@@ -2,6 +2,7 @@ from MoBoAligner import MoBoAligner
 import torch
 
 # Example usage
+torch.manual_seed(1234)
 text_embeddings = torch.randn(2, 5, 10)  # Batch size 2, 5 text tokens, embedding dim 10
 mel_embeddings = torch.randn(2, 800, 10)   # Batch size 2, 8 mel frames, embedding dim 10
 temperature_ratio = 0.5                  # Temperature ratio for Gumbel noise
