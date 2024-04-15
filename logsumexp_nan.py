c9b5214ab9eff6a0622a1759355bed9fb6719aeb
xiaozhah <1298856981@qq.com>
2024-04-15 14:25:11 +0800

    优化错误样例

--- logsumexp_nan.py
+++ logsumexp_nan.py
@@ -1,25 +1,10 @@
 import torch
 
 # 输入包含inf的张量
-x = torch.tensor([-float('inf'), -float('inf'), 1], requires_grad=True)
-y = torch.tensor([-float('inf'), 1, 1], requires_grad=True)
-
-# 计算logsumexp
-output = torch.logsumexp(x + y, dim=0)
-
-# 反向传播
-with torch.autograd.detect_anomaly():
-    output.backward()
-print(x.grad)
-
-######## again ########
-
-# 输入包含inf的张量
 x = torch.tensor([-float('inf'), -float('inf')], requires_grad=True)
-y = torch.tensor([-float('inf'), 1], requires_grad=True)
 
 # 计算logsumexp
-output = torch.logsumexp(x + y, dim=0)
+output = torch.logsumexp(x, dim=0)
 
 # 反向传播
 with torch.autograd.detect_anomaly():
