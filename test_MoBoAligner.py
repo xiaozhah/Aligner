463c9cc81710ac65b19062d138f1308312b808e7
xiaozhah <1298856981@qq.com>
2024-04-23 09:32:45 +0800

    改进测试代码

--- test_MoBoAligner.py
+++ test_MoBoAligner.py
@@ -8,19 +8,21 @@ torch.autograd.set_detect_anomaly(True)
 # Set a random seed to ensure reproducibility of the results
 torch.manual_seed(1234)
 
+I = 2
+J = 4
 # Initialize the text and mel embedding tensors
 text_embeddings = torch.randn(
-    2, 2, 10, requires_grad=True
+    2, I, 10, requires_grad=True
 )  # Batch size: 2, Text tokens: 5, Embedding dimension: 10
 mel_embeddings = torch.randn(
-    2, 4, 10, requires_grad=True
+    2, J, 10, requires_grad=True
 )  # Batch size: 2, Mel frames: 800, Embedding dimension: 10
 # Initialize the text and mel masks
 text_mask = torch.tensor(
-    [[1, 1], [1, 1]], dtype=torch.bool
+    [[1] * I, [1] * I], dtype=torch.bool
 )  # Batch size: 2, Text tokens: 5
 mel_mask = torch.tensor(
-    [[1] * 4, [1] * 4], dtype=torch.bool
+    [[1] * J, [1] * J], dtype=torch.bool
 )  # Batch size: 2, Mel frames: 800
 
 temperature_ratio = 0.5  # Temperature ratio for Gumbel noise
